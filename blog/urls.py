fb096258c1066271c9b16525845197e6f8a19209
valeriykundas <valeriykundas@gmail.com>
2018-05-25 01:40:50 +0300

    more bootstrap added

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
+from django.views.defaults import page_not_found
 
 from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('bootstrap/', views.bootstrap, name='bootstrap'),
+    # path('bootstrap/', views.bootstrap, name='bootstrap'),
     path('about/', views.about, name='about'),
-    path('projects/', views.projects, name='projects'),
+    # path('projects/', views.projects, name='projects'),
     path('posts/', views.posts, name='posts'),
-    path('view/<int:post_id>', views.post, name='post'),
+    path('contact/', views.contact, name='contact'),
+    path('post/<int:post_id>', views.post, name='post'),
     path('', views.main, name='main'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,22 +7,22 @@ from .models import Post
 # Create your views here.
 
 def posts(request):
-    return render(request, "blog/posts.html", {'posts': Post.objects.order_by('-publication_date')})
+    return render(request, 'blog/posts.html', {'posts': Post.objects.order_by('-publication_date')})
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, 'blog/about.html')
 
 def main(request):
-    return render(request, "blog/main.html")
+    return render(request, 'blog/main.html')
 
 def post(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     post.views_count += 1
     post.save()
-    return render(request, "blog/post.html", {'post': post})
-
-def bootstrap(request):
-    return render(request, 'blog/bootstrap.html')
+    return render(request, 'blog/post.html', {'post': post})
 
 def projects(request):
     return render(request, 'blog/projects.html')
+
+def contact(request):
+    return render(request, 'blog/contact.html')
