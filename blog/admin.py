68ed3f5d0e646b3f88fa8b5927df98872bddf229
valeriykundas <valeriykundas@gmail.com>
2018-05-28 14:44:45 +0300

    small fixed to admin Post view

--- blog/admin.py
+++ blog/admin.py
@@ -2,18 +2,17 @@ from django.contrib import admin
 
 from .models import Post, Tag
 
-
 class PostAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Required', {'fields': ['title', 'author', 'text']}),
         ('Attributes', {'fields': ['tags', 'image_url']}),
         ('When to publish', {'fields': ['publication_date']}),
     ]
- #   inlines = [TagInline]
     list_display = ('title', 'author', 'is_published', 'publication_date')
     list_filter = ['publication_date', 'author', 'tags']
     search_fields = ['title', 'author', 'text']
-
+    filter_horizontal = ('tags',)
+    
 
 class TagAdmin(admin.ModelAdmin):
     def get_model_perms(self, request):
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.views import generic
-from django.utils import timezone
 from django.core.paginator import Paginator
+from django.http import HttpResponse
+from django.utils import timezone
+from django.views import generic
 
 from .models import Post
-
-# Create your views here.
 
 
 class AboutView(generic.TemplateView):
