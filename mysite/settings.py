3312bdd52ed3c731439c0da76a79f521afe4beab
valeriykundas <valeriykundas@gmail.com>
2018-05-30 00:15:33 +0300

    updated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,9 @@ DATABASES = {
         'NAME': 'db_mysite',
         'USER': 'postgres',
         'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '',
+        #'HOST': '127.0.0.1',
+        'HOST': 'postgres://tueabssgdrsmnp:82fd57f56d30ac67b49bab9ae7b55c64a6323c359abe786561999f831db76037@ec2-54-235-206-118.compute-1.amazonaws.com',
+        'PORT': '5432',
     },
 }
 
