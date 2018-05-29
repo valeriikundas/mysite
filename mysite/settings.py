007fdf410833a14985c8bfa0546c317245e48c09
valeriykundas <valeriykundas@gmail.com>
2018-05-30 01:10:09 +0300

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'fj6z7&!wn0tsn2pf!#9wqu08$a=*a+oc%tt46u&&5wfdb_tgou'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'thawing-gorge-57580.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'thegreatest.herokuapp.com']
 
 
 # Application definition
@@ -127,3 +127,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
+
+#STATICFILES_DIRS = (os.path.join(BASE_DIR), 'static')
