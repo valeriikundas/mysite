f42b4fd354671c55ed813c0532aa99642a8519cb
valeriykundas <valeriykundas@gmail.com>
2018-05-30 16:45:10 +0300

    added settings/prod.py

---
+++ mysite/settings/prod.py
@@ -0,0 +1,18 @@
+from mysite.settings.base import *
+
+# override settings for production
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['https://thegreatest.herokuapp.com/']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd62r1j5m7aq6t2',
+        'USER': 'tueabssgdrsmnp',
+        'PASSWORD': '82fd57f56d30ac67b49bab9ae7b55c64a6323c359abe786561999f831db76037',
+        'HOST': 'ec2-54-235-206-118.compute-1.amazonaws.com',
+        'PORT': '5432',
+    },
+}
